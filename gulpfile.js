1ae8a56cd028cc8a5ab5b1eed01391d8dfd78286
Martynas Kazlauskas <martynas@firmfirm.co>
2015-09-11 16:53:16 +0300

    add CNAME

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var del = require('del');
 var fs = require('fs');
+var runSequence = require('run-sequence');
 
 gulp.task('clean', function() { return del('.dist') });
 
@@ -12,7 +13,7 @@ gulp.task('build:development', ['clean'], function() {
              .pipe(gulp.dest('.dist'));
 });
 
-gulp.task('build:production', ['clean'], function() {
+gulp.task('build:production:html', function() {
   return gulp.src('src/index.html')
              .pipe($.replace('/* insert:origins */', fs.readFileSync('./src/origins/prod.js')))
              .pipe($.smoosher())
@@ -29,9 +30,16 @@ gulp.task('build:production', ['clean'], function() {
              .pipe(gulp.dest('.dist'));
 });
 
+gulp.task('build:production:copy', function() {
+  return gulp.src('src/CNAME').pipe(gulp.dest('.dist'));
+});
+
+gulp.task('build:production', ['clean'], function(cb) {
+  return runSequence(['build:production:html', 'build:production:copy'], cb);
+});
+
 gulp.task('deploy:production', ['build:production'], function() {
-  return gulp.src('.dist')
-             .pipe($.ghPages());
+  return gulp.src('./.dist/**/*').pipe($.ghPages());
 });
 
 gulp.task('deploy:development', ['build:development'], $.shell.task([
